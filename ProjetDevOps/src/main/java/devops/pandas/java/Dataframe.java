4fbe07a0c711e91807d38491be587b8df46a2935
Ouadfel Soheib <soheib.ouadfel@hotmail.com>
2020-04-20 16:27:36 +0200

    Dataframe constructeur + test v1

---
+++ ProjetDevOps/src/main/java/devops/pandas/java/Dataframe.java
@@ -0,0 +1,30 @@
+package devops.pandas.java;
+import java.util.*; 
+public class Dataframe {
+	ArrayList<List<Object>> datas ;
+	
+	public Dataframe(ArrayList<List<Object>> datasp, List<Object> columns) {
+		datas = new ArrayList();
+		//Remplir la premiere colonne de datas
+		this.datas.add(columns);
+		
+		for(int i=0;i<datasp.size();i++) {
+			datas.add(datasp.get(i));
+		}
+		
+	}
+	
+	public void DisplayData() {
+		for(int i=0; i<datas.size();i++) {
+			for(int j=0;j<datas.get(i).size();j++) {
+				System.out.print(datas.get(i).get(j));
+				System.out.print("\t\t");
+			}
+			System.out.println();
+		}
+	}
+	
+	public ArrayList<List<Object>> getDatas(){
+		return this.datas;
+	}
+}
---
+++ ProjetDevOps/src/test/java/com/devops/ProjetDevOps/DataframeTest.java
@@ -0,0 +1,86 @@
+package com.devops.ProjetDevOps;
+
+import static org.junit.Assert.*;
+
+import java.util.*;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import devops.pandas.java.Dataframe;
+
+public class DataframeTest {
+	private Dataframe dataframe;
+	private static  List<Object> colonnesbase;
+	private  static ArrayList<List<Object>> datasp;
+	
+	@Before
+	public void setUp() {
+		
+		datasp = new ArrayList();
+		colonnesbase = new ArrayList<>();
+		colonnesbase.add("Nom");
+		colonnesbase.add("Prenom");
+		colonnesbase.add("Note");
+        
+        List<Object> ligne1 = new ArrayList<>();
+        ligne1.add("Robert");
+        ligne1.add("Mathieu");
+        ligne1.add(7);
+        List<Object> ligne2 = new ArrayList<>();
+        ligne2.add("Galle");
+        ligne2.add("Chloe");
+        ligne2.add(15);
+        List<Object> ligne3 = new ArrayList<>();
+        ligne3.add("Ouadfel");
+        ligne3.add("Soheib");
+        ligne3.add(12);
+        List<Object> ligne4 = new ArrayList<>();
+        ligne4.add("Guezzal");
+        ligne4.add("Lotfi");
+        ligne4.add(13);
+        List<Object> ligne5 = new ArrayList<>();
+        ligne5.add("Lasnami");
+        ligne5.add("Krimou");
+        ligne5.add(10);
+        List<Object> ligne6 = new ArrayList<>();
+        ligne6.add("Masoudane");
+        ligne6.add("Younes");
+        ligne6.add(17);
+
+        
+      
+        
+        datasp.add(ligne1);
+        datasp.add(ligne2);
+        datasp.add(ligne3);
+        datasp.add(ligne4);
+        datasp.add(ligne5);
+        datasp.add(ligne6);
+        dataframe=new Dataframe(datasp,colonnesbase);
+        
+        
+	}
+
+	   /**
+     * This class method compare each values in a dataframe with the source
+     *
+     * @param df
+     */
+    public static void testConstructor(Dataframe df, int begin, int end) {
+    	//Compare la premiere ligne qui est contient les labels
+    	List<Object> colonnes = df.getDatas().get(0);
+    	//Tester si les deux listes qui correspand au labels ont la meme taille
+    	Assert.assertEquals(colonnesbase.size(),colonnes.size());
+    	
+    	
+    	
+    }
+
+    @Test
+    public void testBasicConstructor() {
+        testConstructor(dataframe, 0, dataframe.getDatas().size() - 1);
+    }
+
+}
